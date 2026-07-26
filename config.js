e3def2aceacaf58a0de567a8b8197451f0c2d733
EdielRodrigues <diel_zi_nho25@hotmail.com>
2026-07-26 09:01:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 window.FINANCE_IA_CONFIG = {
   backendUrl: "https://finance-ia-pro-pix-backend.onrender.com",
-  ownerEmails: ["teste40@hotmail.com"],
+  ownerEmails: ["diel_zi_nho25@hotmail.com"],
   firebase: {
-    apiKey: "AIzaSyCdI11L3Y2GFCy4XzbpSwYGchEtFBzj6Sw",
-    authDomain: "ferramentas-projeto.firebaseapp.com",
-    databaseURL: "https://ferramentas-projeto.firebaseio.com",
-    projectId: "ferramentas-projeto",
-    storageBucket: "ferramentas-projeto.appspot.com",
-    messagingSenderId: "877191590019",
-    appId: ""
+    apiKey: "AIzaSyCJYGxyNmcolcJZZZ4wGX9OiTKro2u-YII",
+  authDomain: "onedraver.firebaseapp.com",
+  databaseURL: "https://onedraver-default-rtdb.firebaseio.com",
+  projectId: "onedraver",
+  storageBucket: "onedraver.appspot.com",
+  messagingSenderId: "228429190925",
+  appId: "1:228429190925:web:655c1d70e51063307f8244",
+  measurementId: "G-8R0SXMWCPV"
   }
 };
