ec4377ca29a5c71de30f9a321414724aecac70e9
EdielRodrigues <diel_zi_nho25@hotmail.com>
2026-08-11 20:55:53 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='finance-ia-pro-v10.1.3';
-const FILES=['./','./index.html','./style.css','./app.js','./v7-features.js','./v10-features.js','./cloud-sync.js','./config.js','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='finance-ia-pro-v10.1.4';
+const FILES=['./','./index.html','./style.css','./app.js','./v7-features.js','./v10-features.js','./cloud-sync.js','./booking-finance.js','./config.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]));});
 self.addEventListener('fetch',event=>{
